c980de2e3ed7e5e08835c7ed6704db43d29ba1bb
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-12-16 23:47:36 -0800

    explain allocator

--- 03-reverse/.tiny/lib.rs
+++ 03-reverse/.tiny/lib.rs
@@ -1,5 +1,8 @@
 use wasm_bindgen::prelude::*;
 
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[wasm_bindgen]
 pub fn reverse(mut xs: Vec<i32>) -> Vec<i32> {
     xs.reverse();
